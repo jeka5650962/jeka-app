b447fbc8fa97f4d21d022b31a93411fd67d2c6b6
Evgeniy Salish <92336813+jeka5650962@users.noreply.github.com>
2022-03-13 13:03:41 +0300

    Откатился на начало домашнего задания

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import Friends from "./components/Friends/Friends";
 
 const App = (props) => {
 
@@ -33,9 +32,6 @@ const App = (props) => {
                         />
                         <Route path="/settings"
                                element={<Settings/>}
-                        />
-                        <Route path="/friends"
-                               element={<Friends/>}
                         />
                     </Routes>
                 </div>
