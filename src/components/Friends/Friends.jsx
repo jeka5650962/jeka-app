17d3570e22d860338328353316cd7a28bd46f2d8
Evgeniy Salish <92336813+jeka5650962@users.noreply.github.com>
2022-03-13 14:00:29 +0300

    Отредактировал Friends.jsx и его стили

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -3,8 +3,28 @@ import style from "./Friends.module.css";
 
 const Friends = () => {
     return (
-        <div>
-            Friends
+        <div className={style.friends}>
+            <h3>Friends</h3>
+            <div className={style.container}>
+                <div className={style.friendItem}>
+                    <img
+                        src="http://sun9-82.userapi.com/sun9-49/s/v1/if1/nd7D6EOnJpznYpfYijdm1Fn884TNbD0HjItMJjX-izZYy5nue-qvZE-TMKX_z4Gq_-L_q--e.jpg?size=256x256&quality=96&crop=0,0,256,256&ava=1"
+                        alt="ava"/>
+                    <div>Dima</div>
+                </div>
+                <div className={style.friendItem}>
+                    <img
+                        src="http://sun9-82.userapi.com/sun9-49/s/v1/if1/nd7D6EOnJpznYpfYijdm1Fn884TNbD0HjItMJjX-izZYy5nue-qvZE-TMKX_z4Gq_-L_q--e.jpg?size=256x256&quality=96&crop=0,0,256,256&ava=1"
+                        alt="ava"/>
+                    <div>Sveta</div>
+                </div>
+                <div className={style.friendItem}>
+                    <img
+                        src="http://sun9-82.userapi.com/sun9-49/s/v1/if1/nd7D6EOnJpznYpfYijdm1Fn884TNbD0HjItMJjX-izZYy5nue-qvZE-TMKX_z4Gq_-L_q--e.jpg?size=256x256&quality=96&crop=0,0,256,256&ava=1"
+                        alt="ava"/>
+                    <div>Andrey</div>
+                </div>
+            </div>
         </div>
     );
 };
